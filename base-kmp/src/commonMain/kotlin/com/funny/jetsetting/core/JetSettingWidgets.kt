122c960e9b9cd81842db4725c1ea4efb60a56ab6
FunnySaltyFish <3036548996@qq.com>
2024-11-03 23:09:35 +0800

    remove containerColor of items in JetSettingDialog

--- base-kmp/src/commonMain/kotlin/com/funny/jetsetting/core/JetSettingWidgets.kt
+++ base-kmp/src/commonMain/kotlin/com/funny/jetsetting/core/JetSettingWidgets.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Switch
@@ -19,6 +20,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import com.funny.data_saver.core.rememberDataSaverState
@@ -231,6 +233,9 @@ fun <E> JetSettingListDialog(
                     trailingContent = {
                         RadioButton(selected = selected == item, onClick = { updateSelected(item) })
                     },
+                    colors = ListItemDefaults.colors(
+                        containerColor = Color.Transparent
+                    )
                 )
             }
         }
