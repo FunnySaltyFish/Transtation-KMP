1e8457d6ccfc51609168cb14ef14ae3fc3824844
FunnySaltyFish <3036548996@qq.com>
2025-01-12 16:38:27 +0800

    Revert "downgrade Kotlin back to 1.9.23"
    This reverts commit 45db05e1bf732f51a4990bc71380d7151dc47a9d.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
-//    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.compose.compiler) apply false
 
 
     alias(libs.plugins.libres) apply false
